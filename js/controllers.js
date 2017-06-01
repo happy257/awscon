d67ffbd419a62179a342e250ec81d74025162752
Abhinav Sachdeva <happy257@gmail.com>
2017-06-01 16:44:37 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,33 @@
-myApp.controller("state1Ctrl",['$scope','$stateParams', function($scope,$stateParams) {
+myApp.controller("state1Ctrl",['$scope','$stateParams','$http',function($scope,$stateParams,$http) {
 	$scope.params=$stateParams;
-	$scope.medContacts=[
-		{id:'car1',name:'Aetna',contactInfo:'1800-120-120'},
-		{id:'car2',name:'Betna',contactInfo:'1800-120-120'},
-		{id:'car3',name:'Cetna',contactInfo:'1800-120-120'},
-	]	
-}])
+	$http({
+		method: 'GET',
+		url:'js/test.php',
+		// 'https://r7rwh2tcgd.execute-api.us-west-2.amazonaws.com/prod/sampleres'
+	})
+	.then(function success(response) {
+		$scope.loading=false;
+		$scope.oth=[
+			{category:"CATEGORY1",telno:"1800-190-280",web:"www.dummyurl.com"},
+			{category:"CATEGORY2",telno:"1800-200-280",web:"www.dummyurl.com"},
+			{category:"CATEGORY3",telno:"1800-390-280",web:"www.dummyurl.com"}
+		] 
+	},function error(response) {
+		console.log(response);
+	});                                                 
+}]);
 myApp.controller("state2Ctrl",['$scope','$state','$http', function($scope,$state,$http) {
 	$scope.loading=true;
 	$http({
 		method: 'GET',
-		url: 'https://r7rwh2tcgd.execute-api.us-west-2.amazonaws.com/prod/sampleres'
+		url://'js/test.php',
+		 'https://r7rwh2tcgd.execute-api.us-west-2.amazonaws.com/prod/sampleres'
 	}).then(function success(response) {
 		$scope.loading=false;
-		$scope.carriers=response.data;
+		//$scope.carriers=response.data;
+		$scope.carriers=[
+			{category:"Medical",carrier:"Aetna",telno:"+91XXXXXXXXX",web:"www.placeholder.com/aetnapagename",searchTips:["Step1 – ","Step2 – ","Step3 – "]},
+		{category:"Medical",carrier:"Betna",telno:"+91XXXXXXXXX",web:"www.placeholder.com/betnapage",searchTips:["Some random text"]},{category:"Medical",carrier:"Cetna",telno:"+91XXXXXXXXX",web:"www.placeholder.com/cetna"}]
 	  }, function error(response) {
 		console.log(response);
 	  });
