281aa47f157adee46e677384830a3f3dde1a1aef
Abhinav Sachdeva <happy257@gmail.com>
2017-06-27 13:09:55 +0530

    Update push.js

--- js/push.js
+++ js/push.js
@@ -1,38 +1,30 @@
-document.addEventListener('deviceready',onDeviceReady,false);
+document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     //alert("device ready")
-    if(typeof PushNotification!='undefined'){
-    var push = PushNotification.init(
-        {"android": {"senderID": "848679895857","forceShow":"true"},
-         "ios": {"alert": "true", "badge": "true", "sound": "true"},
-         "windows": {} 
-    });
-    
-    if(localStorage.getItem("PushMsgRegId")==null){
+    if (typeof PushNotification != 'undefined') {
+        var push = PushNotification.init({"android": {"senderID": "848679895857", "forceShow": "true"}});
+        
         push.on('registration', function(data) {
-            alert("Device registered for push notifications.");
-        //  alert(JSON.stringify(data));
-            localStorage.setItem("PushMsgRegId",data);
-            $("#regId").val(JSON.stringify(data));
+            if(localStorage.getItem("PushMsgRegId")==null) {
+                localStorage.setItem("PushMsgRegId",JSON.stringify(data.registrationId));
+                $("#secretTxt").val(data.registrationId)
+            }else {
+                alert("Device registered for push notifications.\n"+data.registrationId);
+                $("#secretTxt").val(data.registrationId)
+            }
         });
-    }else{
-        $("#regId").val(JSON.stringify(data));
+        
+        push.on('notification', function(data) {
+            alert(JSON.stringify(data));
+            // data.message, data.title, data.count, data.sound, data.image, data.additionalData
+        });
+        
+        push.on('error', function(e) {
+            alert(JSON.stringify(e))
+            // e.message
+        });
     }
-    push.on('notification', function(data) {
-        alert(JSON.stringify(data));
-//        // data.message,
-//        // data.title,
-//        // data.count,
-//        // data.sound,
-//        // data.image,
-//        // data.additionalData
-    });
-
-    push.on('error', function(e) {
-        alert(JSON.stringify(e))
-        // e.message
-    });
-    }else{
+    else {
         alert("PushNotification is undefined");
     }
 }
