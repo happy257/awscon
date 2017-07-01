af482ebaeabbd867a7c07bd5bac3eceba4458daa
Abhinav Sachdeva <happy257@gmail.com>
2017-07-01 17:29:00 +0530

    Update push.js

--- js/push.js
+++ js/push.js
@@ -5,8 +5,8 @@ function onDeviceReady() {
         var push = PushNotification.init({"android": {"senderID": "848679895857", "forceShow": "true"}});
         
         push.on('registration', function(data) {
-            if(localStorage.getItem("PushMsgRegId")==null) {
-                localStorage.setItem("PushMsgRegId",JSON.stringify(data.registrationId));
+            //if(localStorage.getItem("PushMsgRegId")==null) {
+                localStorage.setItem("PushMsgRegId",data.registrationId);
                 $.ajax({
                     url:"https://wys78iyv1d.execute-api.us-east-1.amazonaws.com/prod/dsi_getRegId",
                     method:"POST",
@@ -14,13 +14,14 @@ function onDeviceReady() {
                           "regId": data.registrationId
                         },
                     contentType: "application/json"
-                }).done(function(a,b,c) {
-                    //Register to DynamoDB complete
-                });
-            }else {
+                })
+//                .done(function(a,b,c) {
+//                    alert(c)
+//                });
+            //}else {
                 //Check if device is already registered
                 alert("Device registered for push notifications.\n"+data.registrationId);
-            }
+            //}
         });
         
         push.on('notification', function(data) {
